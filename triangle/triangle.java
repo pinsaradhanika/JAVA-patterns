34a92621a8f66e14474ac498f92b65f14b3a4aac
pinsaradhanika97 <pinsaradhanika@gmail.com>
2021-10-28 21:24:55 +0530

    increment triangle java pattern

---
+++ triangle/triangle.java
@@ -0,0 +1,24 @@
+package triangle;
+
+//increment triangle
+class triangle{
+public static void main(String[] args){
+
+int i,j;
+for(i=0;i<5;i++){
+for(j=0;j<=i;j++){
+System.out.print("*");}
+		
+System.out.println();}
+	}
+}
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
